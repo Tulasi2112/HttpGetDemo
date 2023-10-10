137c1308787df8a61ff620337250e93c7994e0d9
Tulasi Deekonda <tulasideekonda@Tulasis-MacBook-Air.local>
2023-10-10 14:52:21 +0530

    getdemo example

---
+++ GETDemo/GETDemo/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace GETDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            string url = "https://randomuser.me/api/?nat=gb";
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(url);
+            if(response.StatusCode==System.Net.HttpStatusCode.OK)
+            {
+                var jsondata = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(jsondata);
+
+                var myresponse = JsonConvert.DeserializeObject<MyResponse>(jsondata);
+                mynamelabel.Text = myresponse.results.FirstOrDefault().name.first;
+            }
+
+        }
+    }
+}
+
